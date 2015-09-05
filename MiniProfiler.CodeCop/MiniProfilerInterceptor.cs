afaad23dd46c3aac7e0d13a41ff80d2daff88512
dotnetricardo <dotnetricardo@hotmail.com>
2015-09-05 01:38:49 +0100

    Initial commit

---
+++ MiniProfiler.CodeCop/MiniProfilerInterceptor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using CodeCop.Core;
+using CodeCop.Core.Contracts;
+using StackExchange.Profiling;
+
+namespace MiniProfiler.CodeCop
+{
+    public class MiniProfilerInterceptor : ICopIntercept
+    {
+        private readonly StackExchange.Profiling.MiniProfiler _profiler;
+        private IDisposable _step;
+
+        public MiniProfilerInterceptor()
+        {
+            _profiler = StackExchange.Profiling.MiniProfiler.Current;
+        }
+
+        public void OnBeforeExecute(InterceptionContext context)
+        {
+            _step = _profiler.Step(context.InterceptedMethod.Name);
+        }
+
+        public void OnAfterExecute(InterceptionContext context)
+        {
+            if (_step != null)
+            {
+                _step.Dispose();
+                _step = null;
+            }
+        }
+    }
+}
---
+++ MiniProfiler.CodeCop/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MiniProfiler.CodeCop")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MiniProfiler.CodeCop")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4ad17e41-5413-41b5-8820-46e2a5784ae3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
